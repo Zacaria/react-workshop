5791f6fab6ce9e21fc6d14eeec947228dae9a16e
Zacaria <zc@42.am>
2017-05-16 16:11:31 +0200

    [UP] Exercise done

--- src/withLoading.js
+++ src/withLoading.js
@@ -1,1 +1,8 @@
 import React from 'react';
+
+const withLoading = Component => ({loading, data, ...props}) => (
+    loading ? <div {...props}>Loading …</div> :
+    <Component {...props} data={data} loading={loading} />
+);
+
+export default withLoading;
